2f2dc9a5f1dc9b4792174723cf590e590cf550bf
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2024-07-03 14:21:44 +0530

    Update xgemac_sequence_item.sv

--- xgemac_sequence_item.sv
+++ xgemac_sequence_item.sv
@@ -3,8 +3,12 @@
 // This class holds the data items required to drive the stimulus to dut
 //--------------------------------------------------------------------------------------------
 class in_sequence_item extends uvm_sequence_item;
-  
+  //factory registration
   `uvm_object_utils(in_sequence_item)
+  //class constructor
+  function axi_master_transaction::new(string name = "axi_master_transaction");
+    super.new(name);
+  endfunction : new
   
   //-----------------Declaration of signals-----------------
   
@@ -15,7 +19,4 @@ class in_sequence_item extends uvm_sequence_item;
 endclass
 
     
-//--------------------------------------------------------------------------------------------
-function axi_master_transaction::new(string name = "axi_master_transaction");
-  super.new(name);
-endfunction : new
+
