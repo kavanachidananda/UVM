62739b979bd629baaf74842066f21e93f12928e0
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2024-07-05 10:52:05 +0530

    Update xgemac_env.sv

--- xgemac_env.sv
+++ xgemac_env.sv
@@ -26,6 +26,7 @@ class xgemac_env extends uvm_env;
   endfunction
 
   virtual function void connect_phase(uvm_phase phase);
+   super.connect_phase(phase);
    in_agent_h.in_mon_h.in_port.connect(xgemac_scoreboard_h.in_active);
    out_agent_h.out_mon_h.out_got_port.connect(xgemac_scoreboard_h.out_passive);
    wishbone_agent_h.wish_mon_h.wish_port.connect(xgemac_scoreboard_h.wish_active);
