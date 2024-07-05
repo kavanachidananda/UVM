907fcaae19383c4ffe32accffc1f3af0a5b03f1d
kavanachidananda <126802143+kavanachidananda@users.noreply.github.com>
2024-07-05 14:36:41 +0530

    Update xgemac_undersize_vseq.sv

--- xgemac_undersize_vseq.sv
+++ xgemac_undersize_vseq.sv
@@ -2,7 +2,7 @@
 `define XGEMAC_UNDERSIZE_VSEQ_INCLUDED
 
 
-class xgemac_undersize_vseq extends xge;
+class xgemac_undersize_vseq extends xgemac_base_vseq;
 
   //factory registration
   `uvm_object_utils(xgemac_undersize_vseq)
@@ -11,22 +11,23 @@ class xgemac_undersize_vseq extends xge;
 
   extern function new(string name = "xgemac_undersize_vseq");
   extern task body();
-endclass
+    
+endclass : xgemac_undersize_vseq
     
   //class constructor
   function xgemac_undersize_vseq :: new(string name = "xgemac_undersize_vseq");
-  super.new(name);
-endfunction : new
+   super.new(name);
+  endfunction 
 
+  //task body
    task  xgemac_undersize_vseq :: body();
     `uvm_info(get_type_name(), "virtual_seq: Inside Body", UVM_LOW);
-     in_seq_normal_h = in_seq_normal::type_id::create("in_seq_normal_h");
-     
+     in_seq_normal_h = in_seq_normal::type_id::create("in_seq_normal_h"); 
      fork
       in_seq_undersize_h.start(in_seqr_h);
       wish_seq_h.start(wish_seqr_h);
       reset_seq_h.start(reset_seqr_h);
-    join
-      
+    join      
    endtask
+    
 `endif
